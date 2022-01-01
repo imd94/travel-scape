75904ee5a20829ad6a2b9ceb0bf211f27481f48f
Ivan <learner.apprentice@gmail.com>
2022-01-01 22:29:59 +0100

    completed modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,9 +2,10 @@ import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 //import Person from './modules/Person';
 
-
+new Modal();
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 60); 
 //new RevealOnScroll(document.querySelectorAll('.generic-content-container'), 75);
