1cd6d58b9c15379749db8c27a808f32bc0c239ea
Ivan <learner.apprentice@gmail.com>
2022-01-02 19:57:47 +0100

    completed modal part 2

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,16 +2,32 @@ import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
-import Modal from './modules/Modal';
+//import Modal from './modules/Modal';
 //import Person from './modules/Person';
 
-new Modal();
+//new Modal();
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 60); 
 //new RevealOnScroll(document.querySelectorAll('.generic-content-container'), 75);
 
 let mobileMenu = new MobileMenu();
 let stickyHeader = new StickyHeader();
+let modal;
+
+
+document.querySelectorAll('.open-modal').forEach(el => {
+    el.addEventListener('click', e => {
+        e.preventDefault();
+        if(typeof modal == 'undefined') {
+            import(/*webpackChunkName: "modal"*/ './modules/Modal').then(x => {
+                modal = new x.default();
+                setTimeout(() => modal.openTheModal(), 20);
+            }).catch(() => console.log('There was a problem'));
+        } else {
+            modal.openTheModal();
+        }
+    });
+});
 
 
 if(module.hot) {
--- app/assets/scripts/modules/Modal.js
+++ app/assets/scripts/modules/Modal.js
@@ -3,13 +3,13 @@ class Modal {
         this.injectHTML();
         this.modal = document.querySelector('.modal');
         this.closeIcon = document.querySelector('.modal__close');
-        this.openModalButtons = document.querySelectorAll('.open-modal');
+        //this.openModalButtons = document.querySelectorAll('.open-modal');
         this.events();
     }
 
     events() {
         // listen for open click
-        this.openModalButtons.forEach(el => el.addEventListener('click', e => this.openTheModal(e)));
+        //this.openModalButtons.forEach(el => el.addEventListener('click', e => this.openTheModal(e)));
 
         // listen for close click
         this.closeIcon.addEventListener('click', () => this.closeTheModal());
@@ -24,8 +24,12 @@ class Modal {
         }
     }
 
-    openTheModal(e) {
+    /* openTheModal(e) {
         e.preventDefault();
+        this.modal.classList.add('modal--is-visible');
+    } */
+
+    openTheModal() {
         this.modal.classList.add('modal--is-visible');
     }
 
